a8b9f442ee41f30ed2e4807f458d77737ac6f7de
Domenico Sammartino <ziosamma@gmail.com>
2019-07-09 14:22:47 +0200

    Created first tests

--- cypress/integration/PhpTravelsTests.js
+++ cypress/integration/PhpTravelsTests.js
@@ -11,5 +11,27 @@ describe('PHPTRAVELS Test', function() {
         cy.get('button')
         .should('contain','Search')
     })
+
+    it('Check booking options are available', function() {
+       cy.get('a[title="Hotels"]').within(() => {
+            cy.get('span').should('contain', 'Hotels')
+       })
+       cy.get('a[title="Flights"]').within(() => {
+            cy.get('span').should('contain', 'Flights')
+        })
+        cy.get('a[title="Tours"]').within(() => {
+            cy.get('span').should('contain', 'Tours')
+        })
+        cy.get('a[title="Cars"]').within(() => {
+            cy.get('span').should('contain', 'Cars')
+        })
+
+    })
+
+    it('Check when clicking Hotels the form is available', function() {
+        cy.get('a[title="Hotels"]').click()
+
+        cy.get('input[autocomplete="off"]').should('exist')
+    })
     })
 })
